469c6c027f320dc0a22ad5233fa04bb4a21c84e9
lucasd <derhore.lucas@gmail.com>
2022-01-27 16:09:26 +0100

    Final Home

--- dist/index.6765d2ea.js
+++ dist/index.6765d2ea.js
@@ -523,8 +523,45 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "Home", ()=>Home
 );
-const Home = (argument = "")=>{
-    console.log("Home", argument);
+const apiKey = "9898218d421c4e098402407e7f151bd2";
+const Home = ()=>{
+    const preparePage = ()=>{
+        let articles = "";
+        const fetchList = (url)=>{
+            let finalURL = url;
+            fetch(`${finalURL}`).then((response)=>response.json()
+            ).then((response)=>{
+                console.log(response);
+                response.results.forEach((article)=>{
+                    articles += `<div class="cardGame">
+                <img src="${article.background_image}" alt="game image">
+                <h1>${article.name}</h1>
+                
+                <div class="overview">
+                  <h3>${article.name}</h3>
+                  <h1>${article.released}</h1>
+                  <p>Rating: ${article.metacritic}</p>
+                    <a href = "#pagedetail/${article.slug}">${article.slug}</a>
+                </div>
+              </div>
+            `;
+                });
+                document.querySelector(".page-list .articles").innerHTML = articles;
+            });
+        };
+        fetchList(`https://api.rawg.io/api/games?key=${apiKey}&dates=2022-01-01,2022-01-31&page_size=9`);
+    };
+    const render = ()=>{
+        pageContent.innerHTML = `<div class="description">
+      <h3>Recent game releases  2022:</h1>
+    </div>
+      <section class="page-list">
+        <div class="articles">...loading</div>
+      </section>
+    `;
+        preparePage();
+    };
+    render();
 };
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"gkKU3":[function(require,module,exports) {
